f63d469c7f7360f57cc7b28f7de3926b2933621e
kaif225 <100071569+kaif225@users.noreply.github.com>
2024-05-02 16:44:09 +0530

    Update auditTools.groovy

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -1,8 +1,21 @@
-def call() {
-    node {
-        sh """
-            git --version
-            java -version
-           """
+def buildAndTestLaravelApp() {
+    stage('Build Docker image') {
+        steps {
+            script {
+                docker.build('laravel-app', '.')
+            }
+        }
+    }
+    stage('Run PHPUnit tests') {
+        steps {
+            script {
+                docker.image('laravel-app').inside('-v ${PWD}:/var/www/html') {
+                    sh 'composer install --no-interaction --prefer-dist'
+                    sh 'cp .env.example .env'
+                    sh 'php artisan key:generate'
+                    sh 'vendor/bin/phpunit'
+                }
+            }
+        }
     }
 }
