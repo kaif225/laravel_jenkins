d26a484ae3dec946925ace8486342241cc2b213b
kaif225 <100071569+kaif225@users.noreply.github.com>
2024-05-02 17:50:40 +0530

    Update auditTools.groovy

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -1,6 +1,7 @@
 def call() {
     node { 
         sh """ 
+           cd ${WORKSPACE}/laravel_jenkins && \
            git --version && \
            docker build -t laravel2_image .
            """
